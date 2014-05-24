211ca6c731017dc79ce3a759da96d423bbc5902e
Michael Anderson <andermic@gmail.com>
2014-05-24 17:42:02 -0600

    Add photos to admin

--- photocontest/photocontest/admin.py
+++ photocontest/photocontest/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Event, Profile
+from .models import Event, Profile, Photo
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -10,5 +10,10 @@ class ProfileAdmin(admin.ModelAdmin):
     pass
 
 
+class PhotoAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Event, EventAdmin)
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Photo, PhotoAdmin)
